6b7a09ec4677c6dfdae7bccc0c1ec62748081602
Lorena Santos Comar <lorenacomar11@gmail.com>
2025-06-17 23:11:53 -0300

    Final

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     const menuToggle = document.getElementById('menu-toggle');
-    const menuClose = document.getElementById('menu-close'); // Nosso novo botão
+    const menuClose = document.getElementById('menu-close'); 
     const body = document.querySelector('body');
 
     menuToggle.addEventListener('click', function() {
